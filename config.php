b04d3557baf0d8ace07067b36031f05c1103916c
Adel Mahmoud <adelmhmod75@gmail.com>
2024-04-24 15:16:46 +0200

    first commit

--- config.php
+++ config.php
@@ -6,13 +6,6 @@
   const dbName   = "nemrty";
   const userName = "root";
   const passWord = "";
-  /*
-  const URL_ROOT = 'http://aia.edu.eg/nemrty/';
-  const hostName = "localhost";
-  const dbName   = "nemrty";
-  const userName = "adel";
-  const passWord = "j[PUxlcp8XK_";
-  */
   
   require_once("functions/database.php");
 ?>
